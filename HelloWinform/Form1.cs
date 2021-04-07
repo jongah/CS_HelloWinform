5e769a5594654b4f567ba540ce9247d6d7679544
jongah <jongah2021@gmail.com>
2021-04-08 08:59:20 +0900

    add button and event

--- HelloWinform/Form1.cs
+++ HelloWinform/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWinform
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요 밥먹자야아ㅏㅏㅏㅏ");
+        }
     }
 }
